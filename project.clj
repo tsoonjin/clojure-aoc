e26461ca27c48391b8202f136dd9c738811b270e
jinified <jinified@gmail.com>
2021-01-24 11:30:34 +0800

    Completed Day 1

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/tools.cli "1.0.194"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/tools.cli "1.0.194"] [org.clojure/math.combinatorics "0.1.6"]]
 )
--- src/aoc/day1/solve.clj
+++ src/aoc/day1/solve.clj
@@ -1,5 +1,20 @@
-(ns aoc.day1.solve)
+(ns aoc.day1.solve
+  (:require [clojure.math.combinatorics :as combo])
+)
+(require '[clojure.string :as string])
+
+(defn strToNum
+  [xs]
+  "Convert vector of string to vector of int"
+  (map #(Integer/parseInt %) xs)
+)
 
 (defn -main [& args]
-  (println "Day 1")
+  (println "AOC Day 1")
+  (def input (slurp "input1.txt"))
+  (def inputIntegers (strToNum (string/split-lines input)))
+  (def answer1 (first (flatten (map #(* (first %) (last %)) (filter #(= (+ (first %) (last %)) 2020) (combo/combinations inputIntegers 2))))))
+  (println answer1)
+  (def answer2 (first (flatten (map #(apply * %) (filter #(= (apply + %) 2020) (combo/combinations inputIntegers 3))))))
+  (println answer2)
 )
